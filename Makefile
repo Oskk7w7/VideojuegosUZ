ded0e8238e9ba18acf526a496b819a248e86a114
Óscar Jaime <818961@unizar.es>
2026-02-04 23:40:45 +0100

    Reordenado el código en funciones e importado SDL_image (no se usa aún)

--- Makefile
+++ Makefile
@@ -1,5 +1,8 @@
 all: build
-	g++ -I src/include -L src/lib -o build/main src/main.cpp -lmingw32 -lSDL2main -lSDL2
+	g++ -I src/include \
+	    -L src/lib \
+	    -o build/main src/main.cpp \
+	    -lmingw32 -lSDL2main -lSDL2 -lSDL2_image
 
 build:
 	if not exist build mkdir build
--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,53 @@
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
 #include <iostream>
 
-int main(int argc, char* argv[]) {
+//Inicializa SDL, crea la ventana y la superficie
+void inicializarSDL(SDL_Window *win, SDL_Surface *surface) {
 	//Inicializar SDL
 	if (SDL_Init(SDL_INIT_VIDEO) != 0) {
 		std::cerr << "SDL_Init Error: " << SDL_GetError() << std::endl;
-		return 1;
+		exit(1);
 	}
 
 	//Crear ventana
-	SDL_Window *win = SDL_CreateWindow("Hello World!", 100, 100, 640, 480, SDL_WINDOW_SHOWN);
+	win = SDL_CreateWindow("Hello World!", 100, 100, 640, 480, SDL_WINDOW_SHOWN);
 	if (win == nullptr) {
 		std::cerr << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
 		SDL_Quit();
-		return 1;
+		exit(1);
 	}
+
+	//Crear superficie
+	surface = SDL_GetWindowSurface(win);
+	if (surface == nullptr) {
+		std::cerr << "SDL_GetWindowSurface Error: " << SDL_GetError() << std::endl;
+		SDL_Quit();
+		exit(1);
+	}
+}
+
+//Cierra la ventana y termina SDL
+void cerrarSDL(SDL_Window *win, SDL_Surface *surface) {
+	SDL_FreeSurface(surface);	//Liberar superficie
+	surface = NULL;
+	SDL_DestroyWindow(win);		//Cerrar ventana
+	SDL_Quit();					//Cerrar SDL
+}
+
+int main(int argc, char* argv[]) {
+	//Inicializar
+	SDL_Window* win;
+	SDL_Surface* surface;
+	inicializarSDL(win, surface);
+
+
+
+	SDL_UpdateWindowSurface(win);
+
 
 	SDL_Delay(3000); //Esperar 3 segundos para cerrar la ventana
 
-	//Cerrar ventana
-	SDL_DestroyWindow(win);
-	SDL_Quit();
+	cerrarSDL(win, surface);
 	return 0;
 }
